e92bca4bdcc97584a08bf15301d3752f8a3bea79
Сотрудник <gamchanel@gmail.com>
2020-08-31 20:01:05 +0300

    First commit

---
+++ src/com/company/task2/Human.java
@@ -0,0 +1,25 @@
+package com.company.task2;
+
+public class Human {
+    public String name;
+    public int age;
+    public double height;
+    public double weight;
+
+    public Human(String name, int age,
+                 double height,
+                 double weight){
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public void say() {
+        System.out.println(
+                "Name: " + name + "\n" +
+                " Age: " + age +
+                " Height: " + height +
+                " Weight: " + weight);
+    }
+}
---
+++ src/com/company/task2/Main.java
@@ -0,0 +1,13 @@
+package com.company.task2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Human as = new Human(
+                "Dima",
+                13,
+                1.75,
+                65.8);
+        as.say();
+    }
+}
